d2f581ffb5aa56fab89171724766935fd2280b92
MrSofty123 <shaheer1642@gmail.com>
2022-07-12 16:44:25 +0500

    changes

---
+++ build/precache-manifest.78cb9d31e4ea6e3a15c9e4cc74cad7ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2480fa5b651ed3a2d0b1",
+    "url": "/static/css/main.b87f1af0.chunk.css"
+  },
+  {
+    "revision": "2480fa5b651ed3a2d0b1",
+    "url": "/static/js/main.2480fa5b.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "8008a4ba4240d1ac88a4",
+    "url": "/static/js/2.8008a4ba.chunk.js"
+  },
+  {
+    "revision": "ae42267ff6069bcb7427",
+    "url": "/static/js/runtime~main.ae42267f.js"
+  },
+  {
+    "revision": "473f9cdf08c92ab4565f34994993bcfe",
+    "url": "/static/media/grass.473f9cdf.png"
+  },
+  {
+    "revision": "071cf24956dad584284a0149ae3de9b2",
+    "url": "/index.html"
+  }
+];
